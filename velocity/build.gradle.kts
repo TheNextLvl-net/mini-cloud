fc0ce1c2986a1653268ca5a744db4c49a708d9fe
NonSwag <mrminecraft00@gmail.com>
2023-06-10 18:15:52 +0200

    added core annotations

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -7,11 +7,13 @@ version = rootProject.version
 
 repositories {
     mavenCentral()
+    maven("https://repo.thenextlvl.net/releases")
     maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
     compileOnly("org.projectlombok:lombok:1.18.26")
+    compileOnly("net.thenextlvl.core:annotations:1.0.0")
     compileOnly("com.velocitypowered:velocity-api:3.1.1")
 
     annotationProcessor("org.projectlombok:lombok:1.18.26")
