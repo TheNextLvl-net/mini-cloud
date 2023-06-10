cc4926143c50b8b0052eebf8b09556c269cf1c54
NonSwag <mrminecraft00@gmail.com>
2023-06-11 00:23:46 +0200

    added dependencies

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -9,7 +9,13 @@ repositories {
     mavenCentral()
     maven("https://repo.thenextlvl.net/releases")
 }
+
 dependencies {
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    implementation("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
+
     compileOnly("org.jetbrains:annotations:24.0.0")
     compileOnly("net.thenextlvl.core:annotations:1.0.0")
 }
