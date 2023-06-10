8b59f52e0ec850b560a7ef56420574ad3cfa320e
NonSwag <mrminecraft00@gmail.com>
2023-06-10 15:29:47 +0200

    added dependencies

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -7,8 +7,15 @@ version = rootProject.version
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.26")
+    compileOnly("com.velocitypowered:velocity-api:3.1.1")
+
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("com.velocitypowered:velocity-api:3.1.1")
+
     implementation(project(":api"))
 }
