64e264f8373ed04c76534a248867b3c5e56105ff
NonSwag <mrminecraft00@gmail.com>
2023-06-10 13:58:49 +0200

    added core annotations made jetbrains annotations compileOnly

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -7,7 +7,9 @@ version = rootProject.version
 
 repositories {
     mavenCentral()
+    maven("https://repo.thenextlvl.net/releases")
 }
 dependencies {
-    implementation("org.jetbrains:annotations:24.0.0")
+    compileOnly("org.jetbrains:annotations:24.0.0")
+    compileOnly("net.thenextlvl.core:annotations:1.0.0")
 }
