82eb8da645389ea600d91183d6bf3b6b6b06803d
NonSwag <mrminecraft00@gmail.com>
2023-06-10 23:02:03 +0200

    added swagger codegen

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("org.hidetake.swagger.generator") version "2.19.2"
 }
 
 group = "net.thenextlvl.cloud"
@@ -7,4 +8,17 @@ version = "1.0.0"
 
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    swaggerCodegen("org.openapitools:openapi-generator-cli:3.3.4")
+}
+
+swaggerSources {
+    register("easy-cloud") {
+        setInputFile(file("api.yaml"))
+        code.language = "spring"
+        code.wipeOutputDir = true
+        code.outputDir = file("swagger")
+    }
 }
