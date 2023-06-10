065cc9841416fe0f9da3a145e48cd11d549655eb
NonSwag <mrminecraft00@gmail.com>
2023-06-10 18:16:12 +0200

    added package information

---
+++ velocity/src/main/java/net/thenextlvl/cloud/velocity/package-info.java
@@ -0,0 +1,8 @@
+@FieldsAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package net.thenextlvl.cloud.velocity;
+
+import core.annotation.FieldsAreNonnullByDefault;
+import core.annotation.MethodsReturnNonnullByDefault;
+import core.annotation.ParametersAreNonnullByDefault;
