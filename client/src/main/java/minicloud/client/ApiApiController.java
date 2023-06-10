fa1c4a595148ef732e5811b3ce955a5a8e2dc1d6
NonSwag <mrminecraft00@gmail.com>
2023-06-11 01:26:10 +0200

    generated client

---
+++ client/src/main/java/minicloud/client/ApiApiController.java
@@ -0,0 +1,25 @@
+package minicloud.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-06-11T01:25:54.107288741+02:00[Europe/Berlin]")
+
+@Controller
+@RequestMapping("${openapi.miniCloud.base-path:}")
+public class ApiApiController implements ApiApi {
+
+    private final NativeWebRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public ApiApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
---
+++ client/src/main/java/org/openapitools/model/Template.java
@@ -0,0 +1,182 @@
+package org.openapitools.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Template
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-06-11T01:25:54.107288741+02:00[Europe/Berlin]")
+
+public class Template   {
+  @JsonProperty("name")
+  private String name;
+
+  @JsonProperty("command")
+  private String command;
+
+  @JsonProperty("size")
+  private Integer size;
+
+  @JsonProperty("createdAt")
+  private OffsetDateTime createdAt;
+
+  @JsonProperty("updatedAt")
+  private OffsetDateTime updatedAt;
+
+  public Template name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Get name
+   * @return name
+  */
+  @ApiModelProperty(example = "lobby-template", value = "")
+
+@Pattern(regexp="^[a-zA-Z0-9-_]+$") 
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Template command(String command) {
+    this.command = command;
+    return this;
+  }
+
+  /**
+   * Get command
+   * @return command
+  */
+  @ApiModelProperty(example = "java -jar server.jar", value = "")
+
+
+  public String getCommand() {
+    return command;
+  }
+
+  public void setCommand(String command) {
+    this.command = command;
+  }
+
+  public Template size(Integer size) {
+    this.size = size;
+    return this;
+  }
+
+  /**
+   * Get size
+   * @return size
+  */
+  @ApiModelProperty(value = "")
+
+
+  public Integer getSize() {
+    return size;
+  }
+
+  public void setSize(Integer size) {
+    this.size = size;
+  }
+
+  public Template createdAt(OffsetDateTime createdAt) {
+    this.createdAt = createdAt;
+    return this;
+  }
+
+  /**
+   * Get createdAt
+   * @return createdAt
+  */
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public OffsetDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(OffsetDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Template updatedAt(OffsetDateTime updatedAt) {
+    this.updatedAt = updatedAt;
+    return this;
+  }
+
+  /**
+   * Get updatedAt
+   * @return updatedAt
+  */
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public OffsetDateTime getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(OffsetDateTime updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Template template = (Template) o;
+    return Objects.equals(this.name, template.name) &&
+        Objects.equals(this.command, template.command) &&
+        Objects.equals(this.size, template.size) &&
+        Objects.equals(this.createdAt, template.createdAt) &&
+        Objects.equals(this.updatedAt, template.updatedAt);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, command, size, createdAt, updatedAt);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Template {\n");
+    
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    command: ").append(toIndentedString(command)).append("\n");
+    sb.append("    size: ").append(toIndentedString(size)).append("\n");
+    sb.append("    createdAt: ").append(toIndentedString(createdAt)).append("\n");
+    sb.append("    updatedAt: ").append(toIndentedString(updatedAt)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
