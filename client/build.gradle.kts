c0a677df8f8092f6e8b574bd4ec7aa5ea68d5172
NonSwag <mrminecraft00@gmail.com>
2023-06-11 02:16:41 +0200

    replaced compileOnly with implementation

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -15,9 +15,9 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.springframework.boot:spring-boot-starter-web:2.0.1.RELEASE")
-    compileOnly("io.springfox:springfox-swagger2:2.8.0")
-    compileOnly("io.springfox:springfox-swagger-ui:2.8.0")
-    compileOnly("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.0.1.RELEASE")
-    compileOnly("javax.validation:validation-api:2.0.1.Final")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.0.1.RELEASE")
+    implementation("io.springfox:springfox-swagger2:2.8.0")
+    implementation("io.springfox:springfox-swagger-ui:2.8.0")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.0.1.RELEASE")
+    implementation("javax.validation:validation-api:2.0.1.Final")
 }
