b46f684323286111e36127c022424d9bc34a0ede
NonSwag <mrminecraft00@gmail.com>
2023-06-12 20:10:10 +0200

    changed client generator

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("org.hidetake.swagger.generator") version "2.19.2"
+    id("org.openapi.generator") version "6.6.0"
 }
 
 group = rootProject.group
@@ -11,42 +11,21 @@ java {
     sourceCompatibility = JavaVersion.VERSION_19
 }
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    swaggerCodegen("org.openapitools:openapi-generator-cli:6.6.0")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
-
-    implementation("org.springframework.boot:spring-boot-starter-validation:3.1.0")
-    implementation("org.springframework.boot:spring-boot-starter-web:3.1.0")
-    implementation("org.springframework.data:spring-data-commons:3.1.0")
-
-    implementation("org.springdoc:springdoc-openapi-ui:1.7.0")
-
-    implementation("io.springfox:springfox-swagger-ui:3.0.0")
-    implementation("io.springfox:springfox-swagger2:3.0.0")
-
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-
-    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
-
-    implementation("com.google.code.findbugs:jsr305:3.0.2")
-
-    implementation("javax.validation:validation-api:2.0.1.Final")
-}
-
-swaggerSources {
-    register("mini-cloud") {
-        setInputFile(file("api.yaml"))
-        code.language = "spring"
-        code.outputDir = file(".")
-        code.configFile = file("config.json")
-        code.wipeOutputDir = false
-        code.dependsOn("build")
-    }
+tasks.openApiGenerate {
+    generatorName.set("java")
+    library.set("okhttp-gson")
+    inputSpec.set("$rootDir/api.yaml")
+    outputDir.set("$buildDir/generated-client")
+    apiPackage.set("minicloud.client.api")
+    invokerPackage.set("minicloud.client.invoker")
+    modelPackage.set("minicloud.client.model")
+    skipValidateSpec.set(true)
+    logToStderr.set(true)
+    generateAliasAsModel.set(false)
+    enablePostProcessFile.set(true)
+    configOptions.set(mapOf(
+            "outputAsLibrary" to "true",
+            "asyncNative" to "true",
+            "supportStreaming" to "true"
+    ))
 }
