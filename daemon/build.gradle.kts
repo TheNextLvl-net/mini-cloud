858da3138dec39a03dc1c891e19b3af7ff36dacd
NonSwag <mrminecraft00@gmail.com>
2023-06-12 20:09:43 +0200

    added server generator

--- daemon/build.gradle.kts
+++ daemon/build.gradle.kts
@@ -1,10 +1,24 @@
 plugins {
     id("java")
+    id("org.openapi.generator") version "6.6.0"
 }
 
 group = rootProject.group
 version = rootProject.version
 
-repositories {
-    mavenCentral()
+tasks.openApiGenerate {
+    generatorName.set("go-server")
+    inputSpec.set("$rootDir/api.yaml")
+    outputDir.set("$buildDir/generated-server")
+    apiPackage.set("minicloud.server.api")
+    invokerPackage.set("minicloud.server.invoker")
+    modelPackage.set("minicloud.server.model")
+    skipValidateSpec.set(true)
+    logToStderr.set(true)
+    generateAliasAsModel.set(false)
+    enablePostProcessFile.set(true)
+    configOptions.set(mapOf(
+            "outputAsLibrary" to "true",
+            "sourceFolder" to "server"
+    ))
 }
