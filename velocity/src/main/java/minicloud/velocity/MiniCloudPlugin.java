88a2a5b02c5ef485e3452635c379a81bf39d6f62
david <mrminecraft00@gmail.com>
2023-12-10 13:05:09 +0100

    added data path

--- velocity/src/main/java/minicloud/velocity/MiniCloudPlugin.java
+++ velocity/src/main/java/minicloud/velocity/MiniCloudPlugin.java
@@ -2,9 +2,12 @@ package minicloud.velocity;
 
 import com.google.inject.Inject;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
 import lombok.Getter;
 import org.slf4j.Logger;
+
+import java.nio.file.Path;
 
 @Getter
 @Plugin(
@@ -17,10 +20,12 @@ import org.slf4j.Logger;
 public class MiniCloudPlugin {
     private final ProxyServer server;
     private final Logger logger;
+    private final Path path;
 
     @Inject
-    public MiniCloudPlugin(ProxyServer server, Logger logger) {
+    public MiniCloudPlugin(ProxyServer server, Logger logger, @DataDirectory Path path) {
         this.server = server;
         this.logger = logger;
+        this.path = path;
     }
 }
