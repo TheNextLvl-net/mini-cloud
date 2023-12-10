bdb12794ca0fa58b1b0a544a6096461eafac1ba9
david <mrminecraft00@gmail.com>
2023-12-10 13:14:20 +0100

    added maven publish

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,13 +1,16 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = rootProject.group
-version = rootProject.version
+version = "1.0.0"
 
 java {
     targetCompatibility = JavaVersion.VERSION_19
     sourceCompatibility = JavaVersion.VERSION_19
+    withJavadocJar()
+    withSourcesJar()
 }
 
 repositories {
@@ -22,4 +25,20 @@ dependencies {
     compileOnly("net.thenextlvl.core:annotations:2.0.1")
 
     annotationProcessor("org.projectlombok:lombok:1.18.26")
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+    repositories.maven {
+        val branch = if (version.toString().contains("-pre")) "snapshots" else "releases"
+        url = uri("https://repo.thenextlvl.net/$branch")
+        credentials {
+            if (extra.has("RELEASES_USER"))
+                username = extra["RELEASES_USER"].toString()
+            if (extra.has("RELEASES_PASSWORD"))
+                password = extra["RELEASES_PASSWORD"].toString()
+        }
+    }
 }
